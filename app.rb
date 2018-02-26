b53fb3e1032fa44e52a72086b3bfecfc758554d2
Oli <oliver.berry84@gmail.com>
2018-02-26 23:27:02 +0000

    made it go to html

--- app.rb
+++ app.rb
@@ -3,3 +3,9 @@ require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
 require_relative('./models/albums.rb')
 require_relative('./models/artists.rb')
+
+
+get '/' do
+  @albums = Album.all()
+  erb(:index)
+end
